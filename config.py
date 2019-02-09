4d4fcb2ff3592c92f13b4ff9b537d87fc359bffa
abby-shabi <abbyshabi@gmail.com>
2019-02-09 13:07:18 +0300

    modified the manage.py file

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     '''
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://AbbyShabi:dammy@localhost/pitches'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://AbbyShabi:dammy@localhost/pitch'
 
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     #  email configurations
