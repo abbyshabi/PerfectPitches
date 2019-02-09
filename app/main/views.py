dcd006114cea77fa6b54f04d78ff4e3825a52c8b
abby-shabi <abbyshabi@gmail.com>
2019-02-09 18:47:12 +0300

    added update_pic method to views.py

--- app/main/views.py
+++ app/main/views.py
@@ -44,6 +44,19 @@ def update_profile(uname):
 
     return render_template('profile/update.html')
 
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
+
+
+    
 @main.route('/post/<category>')
 def post(category):
     user=User.query.filter_by(id=id).first()
