e918d6e8b86498c7ba67b33835c65f207209c7a8
abby-shabi <abbyshabi@gmail.com>
2019-02-10 10:43:25 +0300

    fixed the forms route

--- app/auth/views.py
+++ app/auth/views.py
@@ -19,7 +19,7 @@ def login():
 
         flash('Invalid username or Password')
 
-    title = "watchlist login"
+    title = "PitchlyPitches login"
     return render_template('auth/login.html',login_form = login_form,title=title)
 
 
@@ -38,7 +38,7 @@ def register():
         db.session.add(user)
         db.session.commit()
 
-        mail_message("Welcome to watchlist","email/welcome_user",user.email,user=user)
+        mail_message("Welcome to PitchyPitches","email/welcome_user",user.email,user=user)
 
         return redirect(url_for('auth.login'))
         title = "New Account"
--- app/main/forms.py
+++ app/main/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField,ValidationError
+from wtforms import StringField,TextAreaField,SubmitField,ValidationError,SelectField
 from wtforms.validators import Required,Email
 #from .models import User
 
@@ -7,7 +7,11 @@ class PostForm(FlaskForm):
 
     title = StringField('Review title',validators=[Required()])
     pitch = TextAreaField('Movie review', validators=[Required()])
-    submit = SubmitField('Submit')
+    post = SubmitField('Submit')
+    category = SelectField(
+        "category",
+        choices=[("pick-up", "pick-up"),("boring","boring"),("funny","funny"),("promotion","promotion"),("product","product"),("cheesy","cheesy"),("random","random")],validators = [Required()]
+    )
 
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [Required()])
